ae00f65f281361d22e6cf367dec0c4bb59fd48b7
Ruhma10x <ruhma.rizwan@10xengineers.com>
2023-04-05 10:33:14 +0500

    Updated Accumulatortests.s

--- lab04/accumulatortests.s
+++ lab04/accumulatortests.s
@@ -1,7 +1,7 @@
 .import lotsofaccumulators.s
 
 .data
-inputarray: .word 0,2,3,4,5,6,7,0
+inputarray: .word 1,2,3,4,5,6,7,0
 
 TestPassed: .asciiz "Test Passed!"
 TestFailed: .asciiz "Test Failed!"
@@ -16,26 +16,37 @@ TestFailed: .asciiz "Test Failed!"
 #Modify the test so that you can catch the bugs in four of the five solutions!
 main:
     la a0 inputarray
-    li s0,4
-    li t2, 5
-    addi sp,sp,-4
-    sw t2,0(sp)
-    jal accumulatorone
-    lw  t2,0(sp)
-    addi sp,sp,4
-    li t0 4
-    bne s0 t0 Fail
-    li t0 5
-    bne t2 t0 Fail
+    li s0,4 #check accumone
+    li t2, 5 #check accumtwo and accumfour
+    addi sp,sp,-4 #check accumfive
+    lw t3,0(a0)   #check accumfive
+    sw a0, 0(sp)   #check accumfive
+    sw x0, 0(a0)   #check accumfive
+    jal accumulatorfive
+    li t0 27   #check accumfive
+    beq a0 t0 Fail   #check accumfive
+    lw a0,0(sp)   #check accumfive
+    sw t3, 0(a0)   #check accumfive
+    addi sp,sp,4   #check accumfive
     
     
     
-    li t0 28
+    addi sp,sp,-4  #check accumtwo
+    sw t2,0(sp)    #check accumtwo
+    jal accumulatorfive
+    lw  t2,0(sp)   #check accumtwo
+    addi sp,sp,4   #check accumtwo
+    li t0 4   #check accumone
+    bne s0 t0 Fail  #check accumone
+    li t0 5   #check accummtwo
+    bne t2 t0 Fail   #check accumtwo
+    
+    li t0 28   #check for pass
     
     
     
     
-    beq a0 t0 Pass
+    beq a0 t0 Pass  #Passed
 Fail:
     la a0 TestFailed
     jal print_string
